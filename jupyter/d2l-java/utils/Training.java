16b844cb22ebbd2963e8afbb7e7b3cf7f7e67758
aksrajvanshi <aksrajvanshi@gmail.com>
2020-07-04 15:58:38 -0400

    Added updatedSGD method in Training class to be used later.

--- jupyter/d2l-java/utils/Training.java
+++ jupyter/d2l-java/utils/Training.java
@@ -1,4 +1,6 @@
-import ai.djl.ndarray.*;
+import ai.djl.ndarray.NDArray;
+import ai.djl.ndarray.NDList;
+import ai.djl.ndarray.types.DataType;
 
 class Training {
 
@@ -21,4 +23,31 @@ class Training {
             param.getGradient().close();
         }
     }
+
+    // Updated SGD function to be used in ipynb files later.
+    public static void sgdUpdated(NDList params, float lr, int batchSize) {
+        for (int i = 0; i < params.size(); i++) {
+            NDArray param = params.get(i);
+            // Update param
+            // param = param - param.gradient * lr / batchSize
+            param.subi(param.getGradient().mul(lr).div(batchSize));
+        }
+    }
+
+    // softmax entropy
+    public static float accuracy(NDArray yHat, NDArray y) {
+        // Check size of 1st dimension greater than 1
+        // to see if we have multiple samples
+        if (yHat.getShape().size(1) > 1) {
+            // Argmax gets index of maximum args for given axis 1
+            // Convert yHat to same dataType as y (int32)
+            // Sum up number of true entries
+            return yHat.argMax(1).toType(DataType.INT32, false).eq(y.toType(DataType.INT32, false))
+                    .sum().toType(DataType.FLOAT32, false).getFloat();
+        }
+        return yHat.toType(DataType.INT32, false).eq(y.toType(DataType.INT32, false))
+                .sum().toType(DataType.FLOAT32, false).getFloat();
+    }
+
+
 }
